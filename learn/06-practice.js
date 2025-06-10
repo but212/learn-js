72676c50bebab898205290d656a9dc3ad1a1feb1
Jeong-il Suk <wjddlf211@naver.com>
2025-06-10 15:06:18 +0900

    add: 변수 실습 추가

---
+++ learn/06-practice.js
@@ -0,0 +1,60 @@
+// 상수 활용
+const hours_per_day = 24;
+console.log("하루는 " + hours_per_day + " 시간입니다.");
+
+const water_freezing_point = 0;
+console.log("물의 어는 점은 " + water_freezing_point + "도 입니다.");
+
+const triangle_angle_sum = 180;
+console.log("삼각형의 내각은 " + triangle_angle_sum + "도 입니다.");
+
+const earth_gravity_accelaration = 9.8;
+console.log(
+  "지구의 중력 가속도는 " + earth_gravity_accelaration + "m/s² 입니다."
+);
+
+// 변수 활용(연산적인 부분)
+let sum = 0;
+console.log("합계: " + sum);
+
+let num1 = 300;
+console.log("num1: " + num1);
+
+let num2 = 50;
+console.log("num2: " + num2);
+
+sum = num1 + num2;
+console.log("합계: " + sum);
+
+// 변수 활용(의미적인 부분)
+let person_name = "석정일";
+console.log("제 이름은 " + person_name + " 입니다.");
+
+let age = 26;
+console.log("제 나이는 " + age + "세 입니다.");
+
+let today_temperature = 23;
+console.log("오늘의 날씨는 " + today_temperature + "도 입니다.");
+
+let cart_item_count = 5;
+console.log("지금 카드 안에는 " + cart_item_count + "개의 상품이 있습니다.");
+
+let home_address = "수원시 장안구 123";
+console.log("저희 집은 " + home_address + "에 있습니다.");
+
+let current_time = "14:17";
+console.log("지금은 " + current_time + " 입니다.");
+
+let is_logged_in = true;
+
+if (is_logged_in === true) {
+  console.log("저는 지금 로그인 상태입니다.");
+} else {
+  console.log("저는 지금 로그아웃 상태입니다.");
+}
+
+let pizza_price = 25000;
+console.log("우리동네 피자 가격은 " + pizza_price + "원 입니다.");
+
+let student_count = 43;
+console.log("지금 학생 수는 " + student_count + "명 입니다.");
