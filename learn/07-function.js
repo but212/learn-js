dbde7e5909a16ddc8f5d1f519658d43f8d18b84d
Jeong-il Suk <wjddlf211@naver.com>
2025-06-10 15:46:56 +0900

    add: 함수 예제추가

---
+++ learn/07-function.js
@@ -0,0 +1,18 @@
+/*
+ *  function make_some_ramen(pot, ramen_specify, additional_ingrediant) {
+ *      let result_ramen = boil_the_water(pot);
+ *      result_ramen += additional_ingrediant.spring_onion;
+ *      result_ramen += ramen_specify.soup;
+ *      result_ramen += ramen_specify.noddles;
+ *      return result_ramen;
+ *  }
+ */
+function drawWater() {
+  console.log("빈 양동이 가져오기");
+  console.log("양동이 들고 우물로 이동");
+  console.log("우물에서 물 길러오기");
+  console.log("집으로 귀가");
+  console.log("물 길어오기 완료!");
+}
+
+drawWater();
