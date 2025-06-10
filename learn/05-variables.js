032ea77711f91b0c79be3030102851ca9d67c82e
Jeong-il Suk <wjddlf211@naver.com>
2025-06-10 13:23:46 +0900

    feat(var): 변수 활용 예제 추가

---
+++ learn/05-variables.js
@@ -0,0 +1,28 @@
+// ----------------
+// 변수(Variable)
+// ----------------
+
+
+// ----------------
+// 의미없는 코드
+// ----------------
+
+// console.log("변수");
+// console.log(4 + 27 + "개");
+
+
+// ----------------
+// 의미있는 코드
+// ----------------
+
+// 처음 사과의 갯수
+let apples_for_now = 4;
+console.log("처음 가진 사과의 갯수: " + apples_for_now);
+
+// 추가로 구매한 사과의 갯수
+let apples_additional = 27;
+console.log("추가로 구매한 사과의 갯수: " + apples_additional);
+
+// 현재 내가 가진 사과의 갯수
+apples_for_now += apples_additional;
+console.log("현재 내가 가진 사과의 갯수: " + apples_for_now);
