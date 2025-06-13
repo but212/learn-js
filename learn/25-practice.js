60e4d195d637b99f171df99eeccf26314ebfeae0
Jeong-il Suk <wjddlf211@naver.com>
2025-06-13 16:24:23 +0900

    exorcistCharactars 활용해서 쿼리 시간 단축

--- learn/25-practice.js
+++ learn/25-practice.js
@@ -1,11 +1,13 @@
 const exorcistCharacters = document.getElementById("exorcist-characters");
 console.log(exorcistCharacters);
 
-const yoHan = document.querySelector(".character.yo-han");
+const yoHan = exorcistCharacters.querySelector(".character.yo-han");
 console.log(yoHan);
 
-const masterChoi = document.querySelector(".character.master-choi");
+const masterChoi = exorcistCharacters.querySelector(".character.master-choi");
 console.log(masterChoi);
 
-const villainGhost = document.querySelector(".character.villain-ghost");
+const villainGhost = exorcistCharacters.querySelector(
+  ".character.villain-ghost"
+);
 console.log(villainGhost);
