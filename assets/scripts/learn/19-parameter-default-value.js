1b2b6d45c555a2aedd95e969c8ae343052825e50
Jeong-il Suk <wjddlf211@naver.com>
2025-06-16 09:12:49 +0900

    style: style 업데이트 및 _로 변경

--- assets/scripts/learn/19-parameter-default-value.js
+++ assets/scripts/learn/19-parameter-default-value.js
@@ -3,15 +3,13 @@
 // --------------------------------------------------------------------------
 
 function getMoney(price, unit) {
-  // unit 매개변수(옵션)
-  // 설정 가능한 값: '달러' 또는 '원화'
-  // 기본 값: '원'
-
-
+  if (!unit) unit = '원'
+  if (unit === '달러') return '$' + price
+  return price + unit
 }
 
-let moneyKR = getMoney(5000 + 2000 + 1500 + 500) // '10000원'
-let moneyUS = getMoney(10.5 + 90 - 20 + 0.25, '$') // '$80.75'
+let moneyKR = getMoney(5000 + 2000 + 1500 + 500) 
+let moneyUS = getMoney(10.5 + 90 - 20 + 0.25, '달러') 
 
 console.log(moneyKR)
 console.log(moneyUS)
