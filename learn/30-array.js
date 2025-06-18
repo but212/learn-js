2874add777a8b2c347092ed579b7552f2e86aba9
Jeong-il Suk <wjddlf211@naver.com>
2025-06-18 10:49:01 +0900

    feat: 배열 예제 추가

---
+++ learn/30-array.js
@@ -0,0 +1,16 @@
+// ----------------------------------------
+// 배열(Array)
+// ----------------------------------------
+
+// 리터럴 방식
+const shoppingBasket = ["대파", "계란", "두부", "시금치", "양파", "케첩"];
+console.log(shoppingBasket);
+
+const numberOfItems = shoppingBasket.length;
+
+console.log("장바구니 아이템 갯수: ", numberOfItems);
+
+// 객체 생성자 메서드 방식
+const numerics = Array("zero", "one", "two", "three", "four", "five");
+
+console.log(numerics);
