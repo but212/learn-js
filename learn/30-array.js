0bebc98257a9a44773764dee3556a65646c6cf2b
Jeong-il Suk <wjddlf211@naver.com>
2025-06-18 11:29:30 +0900

    feat: 배열 접근 방법 추가

--- learn/30-array.js
+++ learn/30-array.js
@@ -14,3 +14,16 @@ console.log("장바구니 아이템 갯수: ", numberOfItems);
 const numerics = Array("zero", "one", "two", "three", "four", "five");
 
 console.log(numerics);
+
+// 이 둘은 동일
+console.log(shoppingBasket[0]);
+console.log(shoppingBasket.at(0));
+// 대신 at은 음수 인덱스가 가능 -1 === n - 1 === last
+console.log(shoppingBasket.at(-1));
+
+// const onlyOneNumberListLiteral = [99];
+// console.log(onlyOneNumberListLiteral.length);
+// const onlyOneNumberList = new Array(99);
+// console.log(onlyOneNumberList.length);
+// const onlyOneNumberListTwoArg = new Array(99, 100);
+// console.log(onlyOneNumberListTwoArg.length);
