4eb1507df11a91af794ce2422860581960282aff
Jeong-il Suk <wjddlf211@naver.com>
2025-06-30 09:32:49 +0900

    feat: 이벤트 리스너 관리(제거 및 한번만 실행하는 예제 추가)

--- learn/48-remove-event-listener.js
+++ learn/48-remove-event-listener.js
@@ -1,24 +1,47 @@
+// {
+//   const buttonGroup = document.querySelector("[role='group']");
+//   const testerButton = buttonGroup.querySelector("[data-role='tester'");
+//   function handleClick() {
+//     console.log("테스트 버튼에 이벤트 연결");
+//   }
+//   buttonGroup.addEventListener("click", (e) => {
+//     const target = e.target.closest("button");
+//     if (target) {
+//       if (target.dataset.role === "add-event") {
+//         console.log("이벤트 연결됨");
+//         testerButton.textContent = "이벤트 추가됨";
+//         testerButton.addEventListener("click", handleClick);
+//       }
+//       if (target.dataset.role === "remove-event") {
+//         console.log("이벤트 제거됨");
+//         testerButton.textContent = "이벤트 제거됨";
+//         testerButton.removeEventListener("click", handleClick);
+//       }
+//     }
+//   });
+// }
+
+// {
+//   const onceButton = document.querySelector("[data-role='once-button']");
+//   let clickedCount = 0;
+//   function handleOnceClick(e) {
+//     clickedCount += 1;
+//     console.log(clickedCount + "번 클릭했습니다.");
+//     e.currentTarget.removeEventListener("click", handleOnceClick);
+//   }
+//   onceButton.addEventListener("click", handleOnceClick);
+// }
+
 {
-  const buttonGroup = document.querySelector("[role='group']");
-  const testerButton = buttonGroup.querySelector("[data-role='tester'");
+  const onceButton = document.querySelector("[data-role='once-button']");
 
-  function handleClick() {
-    console.log("테스트 버튼에 이벤트 연결");
-  }
-  buttonGroup.addEventListener("click", (e) => {
-    const target = e.target.closest("button");
+  let clickedCount = 0;
 
-    if (target) {
-      if (target.dataset.role === "add-event") {
-        console.log("이벤트 연결됨");
-        testerButton.textContent = "이벤트 추가됨";
-        testerButton.addEventListener("click", handleClick);
-      }
-      if (target.dataset.role === "remove-event") {
-        console.log("이벤트 제거됨");
-        testerButton.textContent = "이벤트 제거됨";
-        testerButton.removeEventListener("click", handleClick);
-      }
-    }
-  });
+  onceButton.addEventListener(
+    "click",
+    () => {
+      console.log(++clickedCount + "번 클릭했습니다");
+    },
+    { once: true }
+  );
 }
