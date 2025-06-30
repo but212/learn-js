f7a4f86eb2fad5e2ecb0b3c7dea3ceab9cbfc92f
Jeong-il Suk <wjddlf211@naver.com>
2025-06-30 11:09:04 +0900

    feat: 모달 닫기 기능 실습완료

---
+++ practice/modal-dialog-close/scripts/main.js
@@ -0,0 +1,38 @@
+// {
+//   const body = document.body
+//   const modal_open_button = document.querySelector('main .modal-open-button')
+//   const modal_overlay = document.querySelector('.modal-overlay')
+//   const modal_close_button = modal_overlay.querySelector('.modal-close-button')
+//   modal_open_button.addEventListener('click', () => {
+//     body.classList.add('modal-is-open')
+//   })
+//   modal_close_button.addEventListener('click', closeModalDialog)
+//   function closeModalDialog() {
+//     body.classList.remove('modal-is-open')
+//   }
+// }
+
+// 아래는 모달 다이얼로그가 닫혀야 하는 상황입니다.
+// - 모달 다이얼로그의 닫기 버튼을 클릭할 때
+// - 모달 오버레이(Modal Overlay) 영역을 마우스로 클릭할 때
+// - 닫기 버튼에 초점 이동 후, `Space` 또는 `Enter` 키를 눌렀을 때
+{
+  const body = document.body;
+  const modal_open_button = document.querySelector("main .modal-open-button");
+  const modal_overlay = document.querySelector(".modal-overlay");
+  const modal_close_button = modal_overlay.querySelector(".modal-close-button");
+  modal_open_button.addEventListener("click", () => {
+    body.classList.add("modal-is-open");
+  });
+  modal_close_button.addEventListener("click", closeModalDialog);
+  function closeModalDialog() {
+    body.classList.remove("modal-is-open");
+  }
+  modal_overlay.addEventListener("click", (e) => {
+    if (e.target !== e.currentTarget) {
+      return;
+    } else {
+      body.classList.remove("modal-is-open");
+    }
+  });
+}
