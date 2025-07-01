0728bbf2cc3b9b9ed39a47f20ea65c8458a8372b
Jeong-il Suk <wjddlf211@naver.com>
2025-07-01 14:09:32 +0900

    docs: 출처 표기

--- learn/utils/maybe.js
+++ learn/utils/maybe.js
@@ -1,3 +1,5 @@
+/* Maybe Monad Implement from https://eddmann.com/posts/maybe-in-javascript/ */
+
 /**
  * @namespace Maybe
  * @description A simple implementation of the Maybe monad for safe optional chaining and error avoidance.
@@ -5,25 +7,31 @@
 const Maybe = (function () {
   /**
    * @class
-   * @description Represents a value that exists (non-null, non-undefined).
+   * @classdesc Represents a value that exists (i.e., is not null or undefined).
    * @param {*} x - The value to wrap.
    */
   const Some = function (x) {
+    /** @type {*} */
     this.x = x;
   };
 
   /**
    * Applies a function to the contained value and wraps the result in a new Maybe.
-   * @param {Function} fn - The function to apply.
-   * @returns {Some|None}
+   * @function
+   * @name Some#fmap
+   * @param {Function} fn - Function to apply to the value.
+   * @returns {Some|None} A new Maybe wrapping the result.
    */
   Some.prototype.fmap = function (fn) {
     return Maybe.of(fn(this.x));
   };
 
   /**
-   * Applies a function that returns a Maybe to the contained value (monadic bind).
-   * @param {Function} fn - A function that returns a Maybe.
+   * Applies a function that returns a Maybe to the contained value.
+   * Equivalent to monadic bind or flatMap.
+   * @function
+   * @name Some#bind
+   * @param {Function} fn - Function returning a Maybe.
    * @returns {Some|None}
    */
   Some.prototype.bind = function (fn) {
@@ -31,7 +39,9 @@ const Maybe = (function () {
   };
 
   /**
-   * Returns the string representation of the instance.
+   * Returns a string representation of this Some value.
+   * @function
+   * @name Some#toString
    * @returns {string}
    */
   Some.prototype.toString = function () {
@@ -40,22 +50,40 @@ const Maybe = (function () {
 
   /**
    * @class
-   * @description Represents the absence of a value.
+   * @classdesc Represents the absence of a value.
    */
   const None = function () {};
 
-  /** @returns {None} */
+  /**
+   * Inert fmap for None; always returns None.
+   * @function
+   * @name None#fmap
+   * @returns {None}
+   */
   None.fmap = () => None;
 
-  /** @returns {None} */
+  /**
+   * Inert bind for None; always returns None.
+   * @function
+   * @name None#bind
+   * @returns {None}
+   */
   None.bind = () => None;
 
-  /** @returns {string} */
+  /**
+   * Returns a string representation of None.
+   * @function
+   * @name None#toString
+   * @returns {string}
+   */
   None.toString = () => "None";
 
   return {
     /**
-     * Creates a Maybe from a value.
+     * Wraps a value into a Maybe.
+     * Returns Some if the value is not null/undefined, otherwise returns None.
+     * @function
+     * @name Maybe.of
      * @param {*} x - The value to wrap.
      * @returns {Some|None}
      */
@@ -63,8 +91,10 @@ const Maybe = (function () {
 
     /**
      * Lifts a function to operate within the Maybe context.
+     * @function
+     * @name Maybe.lift
      * @param {Function} fn - A function to lift.
-     * @returns {Function} - A function that returns a Maybe.
+     * @returns {Function} A new function that returns a Maybe.
      */
     lift:
       (fn) =>
