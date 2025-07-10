2694a80624108df918d10aba9066f25a172f6c6a
Jeong-il Suk <wjddlf211@naver.com>
2025-07-10 13:49:22 +0900

    tutorial: 폼필드 예제 추가

--- learn/71-form.js
+++ learn/71-form.js
@@ -1,10 +1,27 @@
 {
   const form = document.querySelector("form");
-  const input = form.querySelector("input");
-
   form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    console.log(input.value);
+    // 폼요소 순환
+    // // 방법 1
+    // for (const elem of form.elements) {
+    //   console.log(elem);
+    // }
+
+    // // 방법 2
+    // Array.from(form.elements).forEach((elem) => {
+    //   console.log(elem);
+    // });
+
+    // console.log(form.elements);
+    // console.log(form.elements[0]);
+    // console.log(form.elements[1]);
+    // console.log(form.elements[2]);
+    // console.log(form.querySelector("button[type='submit']"));
+    // console.log(form.elements[0].value);
+    // console.log(form.elements[1].value);
+    // console.log(form.elements[2].value);
+    // console.log(form.querySelector("button[type='submit']").value);
   });
 }
